23883351a840f415ef8aa08a3f70d1132f8467a1
christie3406 <87799101+christie3406@users.noreply.github.com>
2024-09-15 20:57:38 +0200

    Update Backtesting.py

--- Backtesting.py
+++ Backtesting.py
@@ -5,9 +5,10 @@ import matplotlib.pyplot as plt
 
 
 # Download underlying security historical data
+# eg. for SIE.DE: 2017-5 to 2018-5 is bearish, 2020-5 to 2021-5 is bullish
 ticker = "SIE.DE"
-start_date = '2023-01-01'
-end_date = '2024-01-01'
+start_date = '2020-01-01'
+end_date = '2021-01-01'
 data = yf.download(ticker, start=start_date, end=end_date, interval='1d')
 
 
@@ -15,13 +16,15 @@ data = yf.download(ticker, start=start_date, end=end_date, interval='1d')
 initial_investment = 10000  # Amount of money to start with
 number_of_weeks = 4  # Trading interval (1 = weekly, 2 = bi-monthly, etc)
 T = (number_of_weeks * 7)/365   # Days till expiration
+d = number_of_weeks * 5  # Interval of trading days (5/week) between iteration
 sigma = 0.25  # Assumed constant implied volatility
 r = 0.01  # Assumed constant risk-free rate
 
 # Lists to hold portfolio values
 covered_call_results = [initial_investment]    # covered call strategy
 protective_put_results = [initial_investment]   # protective put strategy
-bull_call_spread_results = [initial_investment]     # bull call spread results
+put_credit_spread_results = [initial_investment]     # put credit spread results
+call_credit_spread_results = [initial_investment]     # call credit spread results
 bah_results = [initial_investment]    # Buy and hold
 
 
@@ -32,7 +35,6 @@ def find_friday():
     while data.index[d] != fridays.index[0]:
         d += 1
     return d
-
 
 # Using black-scholes formula to estimate the price of european options, since yfinance does not provide historical option data
 def black_scholes(S, K, T, r, sigma, option_type='call'):
@@ -48,7 +50,6 @@ def black_scholes(S, K, T, r, sigma, option_type='call'):
 
     return option_price
 
-
 # Buy and hold strategy
 def buy_and_hold():
     shares_bought = initial_investment // data['Close'][find_friday()]
@@ -58,8 +59,7 @@ def buy_and_hold():
         portfolio_value = current_cash + shares_bought * data["Close"][i]
         bah_results.append(portfolio_value)
 
-
-# Covered call strategy
+# Covered call strategy -> bullish market
 def covered_call(moneyness):    # (moneyness = 1 -> ATM, moneyness = 1.05 -> OTM, moneyness = 0.95 -> ITM)
 
     # INITIATION OF STRATEGY
@@ -69,7 +69,7 @@ def covered_call(moneyness):    # (moneyness = 1 -> ATM, moneyness = 1.05 -> OTM
 
     # Sell an option on the first friday and collect premium
     K = moneyness * data["Close"][find_friday()]
-    option_price = black_scholes(S, K, T, r, sigma, option_type='call')
+    option_price = black_scholes(S, K, T, r, sigma, option_type='call') * 100
     current_cash = initial_investment - (shares_bought * S) + option_price
 
     # Append current portfolio value to results
@@ -99,7 +99,7 @@ def covered_call(moneyness):    # (moneyness = 1 -> ATM, moneyness = 1.05 -> OTM
 
             # Sell a new option
             K = moneyness * S
-            option_price = black_scholes(S, K, T, r, sigma, option_type='call')
+            option_price = black_scholes(S, K, T, r, sigma, option_type='call') * 100
             current_cash += option_price
 
         # Append current portfolio value to results
@@ -108,9 +108,8 @@ def covered_call(moneyness):    # (moneyness = 1 -> ATM, moneyness = 1.05 -> OTM
 
     print_results(covered_call_results)
 
-
-# Protective Put Strategy
-def protective_put(moneyness):  # (moneyness = 1 -> ATM, moneyness = 1.05 -> OTM, moneyness = 0.95 -> ITM)
+# Protective Put Strategy -> bearish market
+def protective_put(moneyness):  # (moneyness = 1 -> ATM, moneyness = 1.05 -> ITM, moneyness = 0.95 -> OTM)
 
     # INITIATION OF STRATEGY
     # Buy as many shares as possible in the beginning
@@ -119,7 +118,7 @@ def protective_put(moneyness):  # (moneyness = 1 -> ATM, moneyness = 1.05 -> OTM
 
     # Buy a put option on the first friday to protect against downside risk
     K = moneyness * data["Close"][find_friday()]
-    option_price = black_scholes(S, K, T, r, sigma, option_type='put')
+    option_price = black_scholes(S, K, T, r, sigma, option_type='put') * 100
     current_cash = initial_investment - (shares_bought * S) - option_price  # Subtract option premium from cash
 
     # Append current portfolio value to results
@@ -132,34 +131,125 @@ def protective_put(moneyness):  # (moneyness = 1 -> ATM, moneyness = 1.05 -> OTM
     for i in range(find_friday() + 1, len(data), 1):
         # Check new underlying price
         S = data['Close'][i]
-        if current_cash <= 0:
-            break
 
         # If an option expires on the day
         if (i - find_friday()) % d == 0:
             # Check if the put option is exercised or expired
             if S < K:
                 # Option is in the money; sell stock at strike price
-                current_cash += shares_bought * K
-                shares_bought = 0  # All stocks sold due to put execution
+                current_cash += 100 * K
+                shares_bought -= 100  # Sold 100 stocks from put contract
             else:
                 # Option expired worthless, no stock is sold
                 pass
 
-            # Re-buy shares and buy a new put option for the next period
-            shares_bought = initial_investment // S
-            K = moneyness * S  # New strike price for the next put option
-            option_price = black_scholes(S, K, T, r, sigma, option_type='put')
-            current_cash -= shares_bought * S + option_price  # Subtract stock and option cost
+            # Re-buy underlying security if necessary
+            if shares_bought < 100:
+                shares_bought += current_cash // S
+                current_cash -= shares_bought * S
+
+            # Buy a new option
+            K = moneyness * S
+            option_price = black_scholes(S, K, T, r, sigma, option_type='put') * 100
+            current_cash -= option_price
 
         # Append current portfolio value to results
-        portfolio_value = current_cash + (shares_bought * S) + option_price
+        portfolio_value = current_cash + (shares_bought * S)
         protective_put_results.append(portfolio_value)
 
     print_results(protective_put_results)
 
+# Put credit spread -> bullish market
+def put_credit_spread():
+    S = data["Close"][find_friday()]   # Stock price at first friday
+    mK1 = 0.98  # mK1 * S = K1 (Strike price of short put)
+    mK2 = 0.80  # mK2 * S = K2 (Strike price of long put)
 
+    # Sell a OTM put option at strike K1
+    K1 = S * mK1
+    K1_premium = black_scholes(S, K1, T, r, sigma, option_type='put') * 100
 
+    # Buy a OTM put option at strike K2, where K2 < K1
+    K2 = S * mK2
+    K2_premium = black_scholes(S, K2, T, r, sigma, option_type='put') * 100
+
+    # Append current portfolio value to results
+    current_cash = initial_investment + K1_premium - K2_premium
+    put_credit_spread_results.append(current_cash)
+
+    for i in range(find_friday() + 1, len(data), 1):
+        # Check new underlying price
+        S = data['Close'][i]
+
+        # If an option expires on the day
+        if (i - find_friday()) % d == 0:
+            if S < K1:
+                current_cash = current_cash - (100 * K1) + (100 * S)
+                if S < K2:
+                    current_cash = current_cash + (100 * K2) - (100 * S)
+            else:
+                pass
+
+            # Sell a OTM put option at strike K1
+            K1 = S * mK1
+            K1_premium = black_scholes(S, K1, T, r, sigma, option_type='put') * 100
+
+            # Buy a OTM put option at strike K2, where K2 < K1
+            K2 = S * mK2
+            K2_premium = black_scholes(S, K2, T, r, sigma, option_type='put') * 100
+
+            current_cash = current_cash + K1_premium - K2_premium
+
+        # Append current portfolio value to results
+        put_credit_spread_results.append(current_cash)
+
+    print_results(put_credit_spread_results)
+
+# Call credit spread -> bearish market
+def call_credit_spread():
+    S = data["Close"][find_friday()]   # Stock price at first friday
+    mK1 = 1.05  # mK1 * S = K1 (Strike price of short call)
+    mK2 = 1.20  # mK2 * S = K2 (Strike price of long call)
+
+    # Sell a OTM call option at strike K1
+    K1 = S * mK1
+    K1_premium = black_scholes(S, K1, T, r, sigma, option_type='call') * 100
+
+    # Buy a OTM call option at strike K2, where K2 > K1
+    K2 = S * mK2
+    K2_premium = black_scholes(S, K2, T, r, sigma, option_type='call') * 100
+
+    # Append current portfolio value to results
+    current_cash = initial_investment + K1_premium - K2_premium
+    put_credit_spread_results.append(current_cash)
+
+    for i in range(find_friday() + 1, len(data), 1):
+        # Check new underlying price
+        S = data['Close'][i]
+
+        # If an option expires on the day
+        if (i - find_friday()) % d == 0:
+            if S > K1:
+                current_cash = current_cash + (100 * K1) - (100 * S)
+                if S > K2:
+                    current_cash = current_cash - (100 * K2) + (100 * S)
+            else:
+                pass
+
+            # Sell a OTM call option at strike K1
+            K1 = S * mK1
+            K1_premium = black_scholes(S, K1, T, r, sigma, option_type='call') * 100
+
+            # Buy a OTM call option at strike K2, where K2 > K1
+            K2 = S * mK2
+            K2_premium = black_scholes(S, K2, T, r, sigma, option_type='call') * 100
+
+            current_cash = current_cash + K1_premium - K2_premium
+
+        # Append current portfolio value to results
+        call_credit_spread_results.append(current_cash)
+
+    print_results(call_credit_spread_results)
 
 # Prints weekly returns of the strategy and of a simple buy and hold strategy.
 def print_results(result):
@@ -196,12 +286,12 @@ def print_results(result):
     print(f"Minimum Daily Return: {min_return:.4f}")
     print(f"Annualized Return: {annualized_return:.4f}")
 
-
 # Plot the results of different strategies
 def plot_results():
     plt.plot(covered_call_results, label='Covered Call Strategy')
     plt.plot(protective_put_results, label="Protective Put Strategy")
-    plt.plot(bull_call_spread_results, label="Bull call spread Strategy")
+    plt.plot(put_credit_spread_results, label="Put credit spread Strategy")
+    plt.plot(call_credit_spread_results, label="Call credit spread Strategy")
     plt.plot(bah_results, label='Buy and Hold Strategy', linestyle='--')
     plt.title('Strategy backtesting')
     plt.xlabel('Trading days')
@@ -212,8 +302,12 @@ def plot_results():
 
 buy_and_hold()
 covered_call(moneyness=1)
-bull_call_spread(K1=5, K2=10)
+put_credit_spread()
+protective_put(moneyness=0.9)
+call_credit_spread()
 plot_results()
+
+
 
 
 
